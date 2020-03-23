c75e7730b8bcf23ffebffae7afe47b966e6f35cb
Requena Eric <e.requena.dev@gmail.com>
2020-03-23 15:34:42 +0100

    Initial Commit

---
+++ HelpHospital/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelpHospital
+//
+//  Created by Eric DkL on 23/03/2020.
+//  Copyright © 2020 Eric DkL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
